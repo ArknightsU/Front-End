003f8be6f95fc248f5afcbf3489eb758ed03bdb7
yusa-imit <esimileiar81@gmail.com>
2021-10-06 16:09:51 +0900

    delete grid one

--- src/components/main_page_comp/TestElement.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface TestElementProps {
-    text?: string | undefined;
-}
-
-export function TestElement(props: TestElementProps): JSX.Element {
-    return <div>{props.text}</div>;
-}
