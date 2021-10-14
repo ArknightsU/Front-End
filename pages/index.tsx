1895263d97627c670822fedc8a80fdea54dc3d6f
yusa-imit <esimileiar81@gmail.com>
2021-10-15 00:23:17 +0900

    ads removed from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,51 +7,50 @@ import {
     Music,
     Login,
     Settings,
-    Ads,
     Dev,
     Theme,
     Logo,
     GridItem,
 } from "@components";
 import { Container } from "@components/Container";
+import Script from "next/script";
 import { useWindowSize } from "react-use";
 
 const Home: React.FC = () => {
     return (
-        <Container>
-            <ResponsiveGrid>
-                <GridItem key={"operator"}>
-                    <Operator />
-                </GridItem>
-                <GridItem key={"gacha"}>
-                    <Gacha />
-                </GridItem>
-                <GridItem key={"calc"}>
-                    <Calculation />
-                </GridItem>
-                <GridItem key={"music"}>
-                    <Music />
-                </GridItem>
-                <GridItem key={"login"}>
-                    <Login />
-                </GridItem>
-                <GridItem key={"settings"}>
-                    <Settings />
-                </GridItem>
-                <GridItem key={"ads"}>
-                    <Ads />
-                </GridItem>
-                <GridItem key={"dev"}>
-                    <Dev />
-                </GridItem>
-                <GridItem key={"theme"}>
-                    <Theme />
-                </GridItem>
-                <GridItem key={"logo"}>
-                    <Logo />
-                </GridItem>
-            </ResponsiveGrid>
-        </Container>
+        <>
+            <Container>
+                <ResponsiveGrid>
+                    <GridItem key={"operator"}>
+                        <Operator />
+                    </GridItem>
+                    <GridItem key={"gacha"}>
+                        <Gacha />
+                    </GridItem>
+                    <GridItem key={"calc"}>
+                        <Calculation />
+                    </GridItem>
+                    <GridItem key={"music"}>
+                        <Music />
+                    </GridItem>
+                    <GridItem key={"login"}>
+                        <Login />
+                    </GridItem>
+                    <GridItem key={"settings"}>
+                        <Settings />
+                    </GridItem>
+                    <GridItem key={"dev"}>
+                        <Dev />
+                    </GridItem>
+                    <GridItem key={"theme"}>
+                        <Theme />
+                    </GridItem>
+                    <GridItem key={"logo"}>
+                        <Logo />
+                    </GridItem>
+                </ResponsiveGrid>
+            </Container>
+        </>
     );
 };
 
