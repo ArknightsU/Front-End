82de4d7cbacbb7d3f0e69cba38a1db2515cec1ba
yusa-imit <esimileiar81@gmail.com>
2021-10-16 23:34:36 +0900

    log in popup

--- src/components/GoogleLoginPopUp/index.tsx
+++ src/components/GoogleLoginPopUp/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Transition } from "@headlessui/react";
 import { useLoginPopUp } from "@recoil/hooks";
 import { useClickAway } from "react-use";
@@ -26,12 +26,6 @@ export function GoogleLoginPopUp(): JSX.Element {
     return (
         <Transition
             show={login}
-            enter="transition-transform transform duration-500"
-            enterFrom="transform scale-0"
-            enterTo="transform scale-100"
-            leave="transition-transform duration-500"
-            leaveFrom="transform scale-100"
-            leaveTo="transform scale-0"
             afterEnter={() => {
                 setIsEntered(true);
             }}
@@ -40,21 +34,32 @@ export function GoogleLoginPopUp(): JSX.Element {
             }}
         >
             <div
+                onClick={toggleLoginPopUp}
                 className={`absolute top-0 left-0 h-full w-full bg-transparent flex justify-center items-center ${bgBlur}`}
             >
-                <div
-                    ref={ref}
-                    className="h-1/2 w-1/3 p-4 box-border bg-white dark:bg-gray-700 rounded-lg"
+                <Transition.Child
+                    as={React.Fragment}
+                    enter="transition transform duration-500 "
+                    enterFrom="scale-0 opacity-0 "
+                    enterTo="scale-100 opacity-1 "
+                    leave="transition transform duration-500"
+                    leaveFrom="scale-100 "
+                    leaveTo="scale-0"
                 >
-                    <GoogleLogin
-                        clientId="995739344727-jcl6298reb0703kevu4j550mugnusmu9.apps.googleusercontent.com"
-                        buttonText="Login"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        cookiePolicy={"single_host_origin"}
-                        isSignedIn={true}
-                    />
-                </div>
+                    <div
+                        ref={ref}
+                        className="relative h-1/2 w-1/3 p-4 box-border bg-white dark:bg-gray-700 rounded-lg"
+                    >
+                        <GoogleLogin
+                            clientId="995739344727-jcl6298reb0703kevu4j550mugnusmu9.apps.googleusercontent.com"
+                            buttonText="Login"
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            cookiePolicy={"single_host_origin"}
+                            isSignedIn={true}
+                        />
+                    </div>
+                </Transition.Child>
             </div>
         </Transition>
     );
