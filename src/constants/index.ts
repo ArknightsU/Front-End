2cd59ad04f4e701364fe9ec3606d83f6c0a156dd
yusa-imit <esimileiar81@gmail.com>
2021-10-25 21:47:08 +0900

    add request function

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,12 @@ export const SERVER_URL_GACHA_POOLS =
     "https://gacha-server-7vnjo7nhlq-du.a.run.app/v1/gacha/pools";
 export const SERVER_URL_GACHA =
     "https://gacha-server-7vnjo7nhlq-du.a.run.app/v1/gacha";
+export const GET_GACHA_API_URL = (
+    count: number,
+    code: number,
+    stack: number,
+): string => {
+    return (
+        SERVER_URL_GACHA + `?gacha_count=${count}&code=${code}&stack=${stack}`
+    );
+};
