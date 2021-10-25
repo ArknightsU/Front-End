d5e0423029463f2d9ca4f2a56851cd1aeee92b3b
yusa-imit <esimileiar81@gmail.com>
2021-10-25 21:47:34 +0900

    gacha detail skeleton

--- src/components/gacha_page_component/GachaDetail/DetailChildLayout.tsx
+++ src/components/gacha_page_component/GachaDetail/DetailChildLayout.tsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface LayoutType {
+    initial?: boolean;
+    title?: string;
     children?: React.ReactNode;
 }
-export function DetailChildLayout({ children }: LayoutType): JSX.Element {
-    return <div className="">{children}</div>;
+export function DetailChildLayout({
+    children,
+    initial = false,
+    title = "SET TITLE",
+}: LayoutType): JSX.Element {
+    const [open, setOpen] = useState(initial);
+    return (
+        <div className="w-full h-auto flex flex-col">
+            <div
+                className="w-full h-16 md:h-20 bg-truegray-700 flex flex-col justify-center text-center relative rounded-lg"
+                onClick={() => {
+                    setOpen(!open);
+                }}
+            >
+                <span className="ml-5 h-auto w-auto text-white font-sans text-xl md:text-3xl font-bold flex flex-row item-center">
+                    {title}
+                </span>
+                <div className="absolute w-auto h-full pt-2 pb-2 right-4 transition-all duration-1000">
+                    <Arrow open={open} />
+                </div>
+            </div>
+            <div
+                className={`w-full transition-all bg-white bg-opacity-60 rounded-lg ${
+                    open ? "h-auto p-4" : "h-0 p-0"
+                }`}
+            >
+                {children}
+            </div>
+        </div>
+    );
 }
+
+interface ArrowProps {
+    open: boolean;
+}
+const Arrow: React.FC<ArrowProps> = ({ open }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-full w-auto transition-all"
+            viewBox="0 0 20 20"
+            fill="#fff"
+        >
+            {open ? (
+                <path
+                    fillRule="evenodd"
+                    d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+                    clipRule="evenodd"
+                />
+            ) : (
+                <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                />
+            )}
+        </svg>
+    );
+};
--- src/components/gacha_page_component/GachaDetail/GachaDetail.tsx
+++ src/components/gacha_page_component/GachaDetail/GachaDetail.tsx
@@ -1,19 +1,49 @@
 import { Transition } from "@headlessui/react";
-import React from "react";
+import React, { useState } from "react";
 import { HorizontalGoogleAds } from "@components/common/GoogleAds/HorizontalGoogleAds";
 import { CustomImage } from "@components/common";
 import { DetailChildLayout } from "./DetailChildLayout";
+import axios from "axios";
+import { GET_GACHA_API_URL } from "src/constants";
 
 interface GachaDetailProps {
     poolSelected: boolean;
     focused: number;
     pools: Array<any>;
+    setDoAnimation: React.Dispatch<React.SetStateAction<boolean>>;
+    setGachaData: React.Dispatch<React.SetStateAction<string[]>>;
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    setError: React.Dispatch<React.SetStateAction<boolean>>;
 }
+const DEV_STACK = 10;
 export function GachaDetail(props: GachaDetailProps): JSX.Element {
+    const [loading, setLoading] = useState(false);
+    const doGacha = (count: number) => {
+        props.setLoading(true);
+        axios
+            .get(
+                GET_GACHA_API_URL(
+                    count,
+                    props.pools[props.focused]["code"],
+                    DEV_STACK,
+                ),
+            )
+            .then((res) => {
+                props.setLoading(false);
+                props.setGachaData(res.data.data);
+                props.setDoAnimation(true);
+                console.log(res);
+            })
+            .catch((reason) => {
+                props.setLoading(false);
+                props.setError(true);
+                console.error(reason);
+            });
+    };
     return (
         <div className="absolute w-screen h-screen flex flex-row justify-center items-center overflow-hidden">
             <div
-                className={`w-11/12 h-full md:w-1/2 absolute transition-all duration-1000 z-40 flex flex-col items-start  pt-12 ${
+                className={`w-11/12 h-full md:w-1/2 absolute transition-all duration-1000 z-40 flex flex-col items-start  pt-12  ${
                     props.poolSelected
                         ? "right-6 lg:right-12 xl:right-24"
                         : "-right-full"
@@ -22,7 +52,7 @@ export function GachaDetail(props: GachaDetailProps): JSX.Element {
                 <div className={`w-full h-auto z-50 bg-yellow-500`}>
                     <HorizontalGoogleAds />
                 </div>
-                <div className="w-full h-auto transition-all mt-4 duration-1000 z-40 bg-red-500 flex flex-col">
+                <div className="w-full h-auto transition-all mt-4 duration-1000 z-40 bg-red-500 flex flex-col space-y-6">
                     {/* Orundum, Originite Status Start */}
                     <div className="w-full h-18 md:h-28 flex flex-row justify-evenly items-center mt-8">
                         <div className="h-18 md:h-28 w-44 md:w-60 bg-white bg-opacity-80 relative rounded-xl overflow-hidden flex flex-row drop-shadow-bottom">
@@ -72,7 +102,12 @@ export function GachaDetail(props: GachaDetailProps): JSX.Element {
                         <div className="h-full w-48">
                             <CustomImage src="/ui/UI_GACHA_POOL_ONE_DRAW.webp" />
                         </div>
-                        <div className="h-full w-48">
+                        <div
+                            className="h-full w-48"
+                            onClick={() => {
+                                doGacha(10);
+                            }}
+                        >
                             <CustomImage src="/ui/UI_GACHA_POOL_TEN_DRAW.webp" />
                         </div>
                     </div>
