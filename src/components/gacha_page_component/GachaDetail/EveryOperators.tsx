3e0401c3f558280ef4b7cb5f56abc339cac29e2e
yusa-imit <esimileiar81@gmail.com>
2021-11-08 23:24:03 +0900

    exception added

--- src/components/gacha_page_component/GachaDetail/EveryOperators.tsx
+++ src/components/gacha_page_component/GachaDetail/EveryOperators.tsx
@@ -30,24 +30,32 @@ export function EveryOperators(props: EveryOperatorsProps): JSX.Element {
     }, [props.data.length]);
     return (
         <div className="w-full h-auto flex flex-row items-center justify-start gap-x-4 gap-y-4 flex-wrap font-ibm-korean font-bold text-truegray-800">
-            {Object.keys(data)
-                .sort((a, b) => {
-                    // @ts-ignore
-                    return data[b] - data[a];
-                })
-                .map((char, idx) => (
-                    <div className="w-24 h-auto flex flex-col" key={idx}>
-                        <CharMinify charName={char} />
-                        <div className="w-24 h-auto flex justify-center items-start">
-                            <p className="text-truegray-700 font-ibm-korean font-bold text-base md:text-lg">
-                                {
-                                    // @ts-ignore
-                                    data[char] + "회"
-                                }
-                            </p>
+            {props.data.length === 0 ? (
+                <div className="w-full h-auto p-8 flex justify-center items-center">
+                    <p className="text-xl font-bold">
+                        {"데이터가 존재하지 않습니다."}
+                    </p>
+                </div>
+            ) : (
+                Object.keys(data)
+                    .sort((a, b) => {
+                        // @ts-ignore
+                        return data[b] - data[a];
+                    })
+                    .map((char, idx) => (
+                        <div className="w-24 h-auto flex flex-col" key={idx}>
+                            <CharMinify charName={char} />
+                            <div className="w-24 h-auto flex justify-center items-start">
+                                <p className="text-truegray-700 font-ibm-korean font-bold text-base md:text-lg">
+                                    {
+                                        // @ts-ignore
+                                        data[char] + "회"
+                                    }
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+            )}
         </div>
     );
 }
