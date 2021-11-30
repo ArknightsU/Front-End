c2efff6bc3eae1524203a431aa35e9c1c5f97a25
yusa-imit <esimileiar81@gmail.com>
2021-12-01 01:39:52 +0900

    auth server security change

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -29,13 +29,13 @@ export default async function auth(req, res) {
         credentials.password === process.env.NEXTAUTH_PASSWORD;
 
     const firebaseConfig = {
-        apiKey: "AIzaSyAdL6jyT9UmimxQrTN6Lll7gJUzfFY5bBo",
-        authDomain: "arknightsu.firebaseapp.com",
-        projectId: "arknightsu",
-        storageBucket: "arknightsu.appspot.com",
-        messagingSenderId: "301249403569",
-        appId: "1:301249403569:web:1a89b81dc6e7c0337aa090",
-        measurementId: "G-HS02MS49SE",
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
     };
     const firebaseApp = initializeApp(firebaseConfig);
     const db = getFirestore(firebaseApp);
@@ -59,9 +59,8 @@ export default async function auth(req, res) {
         theme: { colorScheme: "light" },
         providers: [
             GoogleProvider({
-                clientId:
-                    "301249403569-6818astof7ahgagiufg6pl4ls6ja3u7q.apps.googleusercontent.com",
-                clientSecret: "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",
+                clientId: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_SECRET,
             }),
             CredentialProvider({
                 name: "Credentials",
